e2d5ce5e870427cbf6e28fdf3c670134c124e380
lucasuser22 <floresjeanluca@gmail.com>
2024-04-28 17:08:30 -0600

    first-advance

---
+++ Assets/js/firebase/apikey.js
@@ -0,0 +1,12 @@
+ // Configuración de Firebase
+ const firebaseConfig = {
+    apiKey: "AIzaSyBX35jWyoUxj5s5rqOXpSjeR1ntr3miubc",
+    authDomain: "testlogin-3a8cf.firebaseapp.com",
+    projectId: "testlogin-3a8cf",
+    storageBucket: "testlogin-3a8cf.appspot.com",
+    messagingSenderId: "930751800450",
+    appId: "1:930751800450:web:ed51fd448da24e9a71c09b",
+    measurementId: "G-20YZ81N6JZ"
+  };
+
+  
---
+++ Assets/js/firebase/registroFirebase.js
@@ -0,0 +1,29 @@
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Inicializar Firestore
+const db = firebase.firestore();
+
+// Obtener referencia al formulario de registro
+const registroForm = document.getElementById('formUsuario');
+
+// Manejar el evento de envío del formulario
+registroForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+
+    // Obtener el valor del campo de correo electrónico
+    const email = document.getElementById('txtEmail').value;
+
+    // Crear el documento en Firestore con el ID del correo electrónico del usuario
+    db.collection('users').doc(email).set({})
+        .then(() => {
+            console.log("Documento creado en Firestore con el correo electrónico como ID.");
+            alert("Cuenta registrada exitosamente.");
+            registroForm.reset(); // Reiniciar el formulario después del registro exitoso
+        })
+        .catch((error) => {
+            console.error("Error al registrar la cuenta:", error);
+            alert("Error al registrar la cuenta: " + error.message);
+        });
+});
+
